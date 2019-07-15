7f738cd22098e964a9150eaa2340532875765631
Skye <skye@domos.me.uk>
2019-07-15 13:08:05 +0100

    AdminPane is now interface

--- src/AdminPane.php
+++ src/AdminPane.php
@@ -1,7 +1,7 @@
 <?php 
 namespace Tabula\Modules\Admin;
 
-abstract class AdminPane {
+interface AdminPane {
 
     /**
      * Return the body HTML of your pane here
@@ -10,21 +10,21 @@ abstract class AdminPane {
      * 
      * Passed the tabula instance in case you need it
      */
-    public abstract function render(\Tabula\Tabula $tabula): string;
+    public function render(\Tabula\Tabula $tabula): string;
 
     /**
      * Return the name of your admin pane,
      * for the menu
      */
-    public abstract function getName(): string;
+    public function getName(): string;
 
     /**
      * Return a url-friendly slug for your pane
      */
-    public abstract function getSlug(): string;
+    public function getSlug(): string;
 
     /**
      * Return an icon for the menu if you want to
      */
-    public abstract function getIcon(): ?string;
+    public function getIcon(): ?string;
 }
